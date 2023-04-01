ea0654233427eae80ed88b0c352152317ddb3bbd
unknown <jord_ogrady@hotmail.com>
2023-04-01 14:06:38 +1100

    ''

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+
+const {
+    getThoughts,
+    getThoughtByID,
+    addThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughtsController');
+
+//Home route for thoughts /api/thoughts/
+router.route('/')
+.get(getThoughts)
+
+//  Gets the user id for the thoughts api/thoughts/:userId
+router.route('/:userId')
+.post(addThought)
+
+//Gets the thought id for the thoughts /api/thoughts/:thoughtId
+router.route('/:thoughtId')
+.get(getThoughtByID)
+.put(updateThought)
+.delete(deleteThought)
+
+//Gets the reactions to the thoughts /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions')
+.post(addReaction)
+
+//Gets the reaction id for the thoughts /api/thoughts/:thoughtId/reactions/:reactionId
+
+router.route('/:thoughtId/reactions/:reactionId')
+.delete(deleteReaction)
+
+module.exports = router;
